b26951329d0c25e616a0f90c49bbc23caa7f985a
Geoffrey Hofman-Frethem <gfrethem@gmail.com>
2015-09-29 17:33:46 -0500

    In Progress. Got Button to register click.

--- client/app.js
+++ client/app.js
@@ -1,3 +1,51 @@
 /**
  * Created by gfrethem on 9/29/15.
  */
+var app = angular.module('twitterGenerator', []);
+
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+
+$scope.adjsList = [];
+$scope.nounList = [];
+$scope.handleList = [];
+
+    $http({
+        method: 'GET',
+        url: '/getNouns'
+    }).then(function (response) {
+       $scope.nounList = response.data;
+    });
+
+    $http({
+        method: 'GET',
+        url: '/getAdjs'
+    }).then(function (response) {
+       $scope.adjsList = response.data;
+    });
+
+
+    var randomNumber = function(min, max) {
+        return Math.floor(Math.random() * (1 + max - min) + min);
+    };
+
+    function generateHandle() {
+        $scope.handle = $scope.adjsList[randomNumber(0, $scope.adjsList.length)] + $scope.nounList[randomNumber(0, $scope.nounList.length)];
+        return $scope.handle;
+    }
+
+
+    $scope.kickOff = function(num) {
+        console.log('Kicked');
+        $scope.handleList = [];
+        for (var i = 0; i < num; i++) {
+            $scope.handleList.push(generateHandle());
+            //console.log(handleList);
+        }
+        return $scope.handleList;
+    };
+
+
+
+
+
+}]);
--- server/app.js
+++ server/app.js
@@ -3,11 +3,23 @@
  */
 var express = require('express');
 var app = express();
+var nouns = require('./public/assets/data/nouns');
+var adjs = require('./public/assets/data/adjectives');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + "/public/assets/views/index.html");
+});
+
+app.get('/getNouns', function(request, response) {
+    response.send(nouns);
+    console.log("Sent nouns.json");
+});
+
+app.get('/getAdjs', function(request, response) {
+    response.send(adjs);
+    console.log("Sent adjectives.json");
 });
 
 var server = app.listen(3000, function() {
